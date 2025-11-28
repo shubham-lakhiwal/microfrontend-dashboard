d41a2fa53a952f3cd3d12c0a52a194b85ea5efce
Shubham Lakhiwal <shubham.lakhiwal@gmail.com>
2025-11-28 16:05:46 +0530

    classnames made unique

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import {BrowserRouter} from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'cont'
+})
 export default function App() {
-  console.log("App======")
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
