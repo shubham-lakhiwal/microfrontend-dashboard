e3e9db3fee12eee3b8a5d07b9bb17657f7a70717
Shubham Lakhiwal <shubham.lakhiwal@gmail.com>
2025-11-28 10:10:27 +0530

    Added workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MarketingApp from "./components/MarketingApp";
 
 export default function App() {
+  console.log("Appppppp")
   return (
     <div>
       <h1>Marketing App Container</h1>
