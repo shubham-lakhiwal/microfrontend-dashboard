9add6546572d2720ff4325af2ccf08891cce5d80
Shubham Lakhiwal <shubham.lakhiwal@gmail.com>
2025-11-29 09:32:42 +0530

    Performance

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import './styles.css'
 
-import LandingPage from "./components/Landing";
-import PricingPage from "./components/Pricing";
+const LandingPage = lazy(() => import('./components/Landing'));
+const PricingPage = lazy(() => import('./components/Pricing'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'mark'
@@ -14,10 +14,12 @@ export default function App({history}) {
   return <div className="mkt">
     <StylesProvider generateClassName={generateClassName}>
       <Router history={history}>
-        <Switch>
-          <Route exact path="/pricing" component={PricingPage} />
-          <Route path="/" component={LandingPage} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/pricing" component={PricingPage} />
+            <Route path="/" component={LandingPage} />
+          </Switch>
+        </Suspense>
       </Router>
     </StylesProvider>
   </div>
